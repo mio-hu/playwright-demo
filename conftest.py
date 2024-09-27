45c76f56e43bafbf5f69dc0cea850efb5620d9d8
mio <mio@zbanx.com>
2024-09-27 18:39:05 +0800

    - 新增了用例失败重试策略

--- conftest.py
+++ conftest.py
@@ -1,8 +1,18 @@
 import re
+import hashlib
+import sys
+
+import allure
 import pytest
-from playwright.sync_api import Browser, BrowserContext, expect
-from pytest_playwright.pytest_playwright import ArtifactsRecorder, CreateContextCallback
+import tempfile
+import os
+import shutil
+from pathlib import Path
+from slugify import slugify
+from playwright.sync_api import Browser, BrowserContext, expect, Playwright, Page, Error, sync_playwright
+from pytest_playwright.pytest_playwright import CreateContextCallback
 from utils.global_map import GlobalMap
+from utils import path
 from typing import (
     Any,
     Callable,
@@ -21,12 +31,6 @@ from typing import (
 
 
 
-# @pytest.fixture()
-# def page(page):
-#     cookies = get_cookies(page, base_config.get("username"), base_config.get("password"))
-#     cookies = {"Cookie": "; ".join(f"{cookie['name']}={cookie['value']}" for cookie in cookies)}
-#     page.set_extra_http_headers(cookies)
-#     yield page
 
 @pytest.fixture(scope='session', autouse=True)
 def test_init(base_url):
@@ -66,6 +70,20 @@ def pytest_addoption(parser: Any) -> None:
         default=30_000,
         help="locator timeout and expect timeout",
     )
+    group.addoption(
+        "--rerun_strategy",
+        action="store",
+        default=None,
+        # 这里不使用nargs=“*”是因为无限个args对参数位置有要求，或者测试目标需要用参数指定
+        help="testcase rerun strategy set, eg: screenshot=retain-on-failure,video=retain-on-failure,tracing=retain-on-failure",
+    )
+    group.addoption(
+        "--allure_report_auto_open",
+        action="store",
+        default="off",
+        choices=["off", "on"],
+        help="if finish test, allure report auto open",
+    )
 
 @pytest.fixture(scope="session")
 def ui_timeout(pytestconfig):
@@ -73,13 +91,29 @@ def ui_timeout(pytestconfig):
     expect.set_options(timeout=timeout)
     return timeout
 
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    report = outcome.get_result()
+    if report.failed:
+        try:
+            for content in item.funcargs['browser'].contexts:
+                for page in content.pages:
+                    if page.is_closed():
+                        continue
+                    bytes_png = page.screenshot(timeout=5_000, full_page=True)
+                    allure.attach(bytes_png, f'失败截图---{page.title}')
+        except:
+            ...
 @pytest.fixture
 def new_context(
     browser: Browser,
     browser_context_args: Dict,
     _artifacts_recorder: "ArtifactsRecorder",
     request: pytest.FixtureRequest,
-    ui_timeout,
+    ui_timeout: float,
+    pytestconfig: Any,
+    _pw_artifacts_folder: tempfile.TemporaryDirectory,
 ) -> Generator[CreateContextCallback, None, None]:
     browser_context_args = browser_context_args.copy()
     context_args_marker = next(request.node.iter_markers("browser_context_args"), None)
@@ -88,23 +122,253 @@ def new_context(
     contexts: List[BrowserContext] = []
 
     def _new_context(**kwargs: Any) -> BrowserContext:
-        context = browser.new_context(**browser_context_args, **kwargs)
-        context.set_default_timeout(ui_timeout)
-        context.set_default_navigation_timeout(ui_timeout * 2)
-        original_close = context.close
+        # 获取重试的log策略并转成列表
+        _rerun_strategy = pytestconfig.getoption("--rerun_strategy").split(",")
+        # 获取重试次数，此处为2则为重试两次，加上第一次，一共跑三次
+        _reruns = pytestconfig.getoption("--reruns")
+        video_option = pytestconfig.getoption("--video")
+        # 重试log策略（默认为None）和重试次数（默认为0）参数必须都有值
+        if _rerun_strategy and _reruns:
+            # 使用空字符串去补足轮次和策略的对应关系
+            if _reruns + 1 > len(_rerun_strategy):
+                _init_rerun_strategy = [""] * (1 + _reruns - len(_rerun_strategy)) + _rerun_strategy
+            # 使用切片来处理多余的策略（如果相等，则切片是本身），钶根据自身设计改成从后往前切
+            else:
+                _init_rerun_strategy = _rerun_strategy[:_reruns + 1]
+            # 这里减1是因为request.node.execution_count从1开始，我门取列表下表是从0开始
+            rerun_round = request.node.execution_count - 1
+            _round_rerun_strategy = _init_rerun_strategy[rerun_round]
 
-        def _close_wrapper(*args: Any, **kwargs: Any) -> None:
-            contexts.remove(context)
-            _artifacts_recorder.on_will_close_browser_context(context)
-            original_close(*args, **kwargs)
+            # 这里先判断是否有log策略
+            if _round_rerun_strategy:
+                if "video" in _round_rerun_strategy:
+                    video_option = _round_rerun_strategy.split("=")[-1]
+                else:
+                    video_option = "off"
+            else:
+                video_option = "off"
+        # 这里只判断了video，是因为创建context时必须设置record_video_dir后才开始主动录屏
+        capture_video = video_option in ["on", "retain-on-failure"]
+        browser_context_args.update(kwargs)
+        if capture_video:
+            video_option_dict = {"record_video_dir": _pw_artifacts_folder.name}
+            browser_context_args.update(video_option_dict)
 
-        context.close = _close_wrapper
-        contexts.append(context)
-        _artifacts_recorder.on_did_create_browser_context(context)
-        return context
+        my_context = browser.new_context(**browser_context_args)
+        my_context.set_default_timeout(ui_timeout)
+        my_context.set_default_navigation_timeout(ui_timeout * 2)
+        original_close = my_context.close
+
+        def _close_wrapper(*args: Any, **my_kwargs: Any) -> None:
+            contexts.remove(my_context)
+            _artifacts_recorder.on_will_close_browser_context(my_context)
+            original_close(*args, **my_kwargs)
+
+        my_context.close = _close_wrapper
+        contexts.append(my_context)
+        _artifacts_recorder.on_did_create_browser_context(my_context)
+        return my_context
 
     yield cast(CreateContextCallback, _new_context)
     for context in contexts.copy():
         context.close()
 
 
+def truncate_file_name(file_name: str) -> str:
+    if len(file_name) < 256:
+        return file_name
+    return f"{file_name[:100]}-{hashlib.sha256(file_name.encode()).hexdigest()[:7]}-{file_name[-100:]}"
+
+def _build_artifact_test_folder(
+    pytestconfig: Any, request: pytest.FixtureRequest, folder_or_file_name: str
+) -> str:
+    output_dir = pytestconfig.getoption("--output")
+    return os.path.join(
+        output_dir,
+        truncate_file_name(slugify(request.node.nodeid)),
+        truncate_file_name(folder_or_file_name),
+    )
+
+class ArtifactsRecorder:
+    def __init__(
+        self,
+        pytestconfig: Any,
+        request: pytest.FixtureRequest,
+        playwright: Playwright,
+        pw_artifacts_folder: tempfile.TemporaryDirectory,
+    ) -> None:
+        self._request = request
+        self._pytestconfig = pytestconfig
+        self._playwright = playwright
+        self._pw_artifacts_folder = pw_artifacts_folder
+
+        self._all_pages: List[Page] = []
+        self._screenshots: List[str] = []
+        self._traces: List[str] = []
+        self._rerun_strategy = pytestconfig.getoption("--rerun_strategy").split(",")
+        self._reruns = pytestconfig.getoption("--reruns")
+        if self._rerun_strategy and self._reruns:
+            if self._reruns + 1 >= len(self._rerun_strategy):
+                self._init_rerun_strategy = [""] * (1 + self._reruns - len(self._rerun_strategy)) + self._rerun_strategy
+            else:
+                self._init_rerun_strategy = self._rerun_strategy[:self._reruns + 1]
+
+            rerun_round = request.node.execution_count - 1
+            self._round_rerun_strategy = self._init_rerun_strategy[rerun_round]
+
+            #  以下为判断log策略内容和参数的方法,注意,如果没有则设置为off
+            if "screenshot" in self._round_rerun_strategy:
+                self._screenshot_option = self._round_rerun_strategy.split("=")[-1]
+            else:
+                self._screenshot_option = "off"
+            if "video" in self._round_rerun_strategy:
+                self._video_option = self._round_rerun_strategy.split("=")[-1]
+            else:
+                self._video_option = "off"
+            if "tracing" in self._round_rerun_strategy:
+                self._tracing_option = self._round_rerun_strategy.split("=")[-1]
+            else:
+                self._tracing_option = "off"
+            self._capture_trace = self._tracing_option in ["on", "retain-on-failure"]
+        else:
+            #  没有重试log策略和重试次数,自然取原始的log策略
+            self._screenshot_option = self._pytestconfig.getoption("--screenshot")
+            self._video_option = self._pytestconfig.getoption("--video")
+            self._tracing_option = pytestconfig.getoption("--tracing")
+            self._capture_trace = self._tracing_option in ["on", "retain-on-failure"]
+
+    def did_finish_test(self, failed: bool) -> None:
+        #  获取当前轮次并初始化一个字符串,给保存文件做前缀
+        round_prefix = f"round{self._request.node.execution_count}-"
+        #  这里可以学习一下组合的布尔逻辑
+        capture_screenshot = self._screenshot_option == "on" or (
+                failed and self._screenshot_option == "only-on-failure"
+        )
+        if capture_screenshot:
+            for index, screenshot in enumerate(self._screenshots):
+                human_readable_status = "failed" if failed else "finished"
+                screenshot_path = _build_artifact_test_folder(
+                    self._pytestconfig,
+                    self._request,
+                    #  原始为 f"test-{human_readable_status}-{index + 1}.png",
+                    f"{round_prefix}{index + 1}-{human_readable_status}-{screenshot.split(os.sep)[-1]}.png",
+                )
+                #  这里这种写法注意下,如果自己需要放log,用这个方式创建很好
+                os.makedirs(os.path.dirname(screenshot_path), exist_ok=True)
+                shutil.move(screenshot, screenshot_path)
+                # allure附加图片文件的方法
+                allure.attach.file(screenshot_path, f"{round_prefix}{index + 1}-{human_readable_status}-{screenshot.split(os.sep)[-1]}.png")
+        else:
+            for screenshot in self._screenshots:
+                os.remove(screenshot)
+
+        if self._tracing_option == "on" or (
+                failed and self._tracing_option == "retain-on-failure"
+        ):
+            for index, trace in enumerate(self._traces):
+                trace_file_name = (
+                    f"{round_prefix}trace.zip" if len(self._traces) == 1 else f"{round_prefix}trace-{index + 1}.zip"
+                )
+                trace_path = _build_artifact_test_folder(
+                    self._pytestconfig, self._request, trace_file_name
+                )
+                os.makedirs(os.path.dirname(trace_path), exist_ok=True)
+                shutil.move(trace, trace_path)
+                # allure附加zip文件的方法
+                allure.attach.file(trace_path, "trace.playwright.dev", extension="zip")
+        else:
+            for trace in self._traces:
+                os.remove(trace)
+
+        preserve_video = self._video_option == "on" or (
+                failed and self._video_option == "retain-on-failure"
+        )
+        if preserve_video:
+            for index, page in enumerate(self._all_pages):
+                video = page.video
+                if not video:
+                    continue
+                try:
+                    video_file_name = (
+                        f"{round_prefix}video.webm"
+                        if len(self._all_pages) == 1
+                        else f"{round_prefix}video-{index + 1}.webm"
+                    )
+                    video.save_as(
+                        path=_build_artifact_test_folder(
+                            self._pytestconfig, self._request, video_file_name
+                        )
+                    )
+                    # allure附加webm录像的方法
+                    allure.attach.file(_build_artifact_test_folder(
+                        self._pytestconfig, self._request, video_file_name
+                    ), "过程录像", allure.attachment_type.WEBM)
+                except Error:
+                    # Silent catch empty videos.
+                    pass
+        else:
+            for page in self._all_pages:
+                # Can be changed to "if page.video" without try/except once https://github.com/microsoft/playwright-python/pull/2410 is released and widely adopted.
+                if self._video_option in ["on", "retain-on-failure"]:
+                    try:
+                        page.video.delete()
+                    except Error:
+                        pass
+
+    def on_did_create_browser_context(self, context: BrowserContext) -> None:
+        context.on("page", lambda page: self._all_pages.append(page))
+        if self._request and self._capture_trace:
+            context.tracing.start(
+                title=slugify(self._request.node.name),
+                screenshots=True,
+                snapshots=True,
+                sources=True,
+            )
+
+    def on_will_close_browser_context(self, context: BrowserContext) -> None:
+        if self._capture_trace:
+            trace_path = Path(self._pw_artifacts_folder.name) / create_guid()
+            context.tracing.stop(path=trace_path)
+            self._traces.append(str(trace_path))
+        else:
+            context.tracing.stop()
+
+        if self._screenshot_option in ["on", "only-on-failure"]:
+            for page in context.pages:
+                try:
+                    screenshot_path = (
+                        # Path(self._pw_artifacts_folder.name) / create_guid()
+                        Path(self._pw_artifacts_folder.name) / page.title()
+                    )
+                    page.screenshot(
+                        timeout=5000,
+                        path=screenshot_path,
+                        full_page=self._pytestconfig.getoption(
+                            "--full-page-screenshot"
+                        ),
+                    )
+                    self._screenshots.append(str(screenshot_path))
+                except Error:
+                    pass
+
+
+def create_guid() -> str:
+    return hashlib.sha256(os.urandom(16)).hexdigest()
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session):
+    if session.config.getoption("--allure_report_auto_open") == "on":
+        if sys.platform != "linux":
+            import subprocess
+            allure_report_dir = session.config.getoption("--alluredir")
+            # 尝试关闭可能已经在运行的Allure 服务
+            try:
+                if sys.platform == "darwin":
+                    subprocess.call("pkill -f 'allure'", shell=True)
+                elif sys.platform == "win32":
+                    command = "taskkill /F /IM allure.exe /T"
+                    subprocess.call(command, shell=True)
+            except Exception as e:
+                print(e)
+            allure_command = f'allure serve {allure_report_dir}'
+            subprocess.Popen(allure_command, shell=True)
